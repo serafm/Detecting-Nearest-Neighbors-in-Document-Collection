12c9e5d906908c7a4c8f2f4d7889d4696b6b9cbf
SeraFM <cs04555@uoi.gr>
2022-04-01 01:10:16 +0300

    UPDATE

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 
+import numpy as np
+
 
 def MyReadDataRoutine():
 
@@ -7,7 +9,38 @@ def MyReadDataRoutine():
     #dataFilePath = sys.argv[1]
     #numDocuments = sys.argv[2]
 
-    dataFilePath = "data/DATA_1-vocab.enron.txt"
+    file = open("data/DATA_1-docword.enron.txt")
     numDocuments = 100
 
+    data = file.read()
+    data = data.split("\n")
 
+    # remove the first 3 lines
+    data.pop(0)
+    data.pop(0)
+    data.pop(0)
+
+    frozenData = []
+
+    for i in range(len(data)-1):
+        list = data[i].split(" ")
+        docID = list[0]
+        wordID = list[1]
+        frozenData = frozenData + [docID, wordID]
+
+    frozeSet = frozenset(frozenData)
+    print(frozeSet)
+
+
+
+
+
+
+
+
+
+def main():
+    MyReadDataRoutine()
+
+if __name__ == "__main__":
+    main()
