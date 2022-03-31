ba0919021eb33255277027a286f8b82ff5f6b283
SeraFM <cs04555@uoi.gr>
2022-04-01 01:52:14 +0300

    UPDATE

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import sys
 
-import numpy as np
 
+def listToString(s):
+    # initialize an empty string
+    str1 = ""
+
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+
+        # return string
+    return str1
 
 def MyReadDataRoutine():
 
@@ -9,49 +18,67 @@ def MyReadDataRoutine():
     #dataFilePath = sys.argv[1]
     #numDocuments = sys.argv[2]
 
+    # file to read the data
     file = open("data/DATA_1-docword.enron.txt")
-    numDocuments = 5
+
+    # new file to write later
+    newFile = open("FrozenDataSet.txt", "w")
+
+    # number of documents to read
+    numDocuments = 10
+
     docs = 1
-    docID = 1
-    nextDocID = 1
+    previousDocID = 1
 
     data = file.read()
     data = data.split("\n")
 
-    # remove the first 3 lines
+    # remove the first 3 lines we don't need them here
     data.pop(0)
     data.pop(0)
     data.pop(0)
 
     frozenData = []
+    string = []
 
     for i in range(len(data)-1):
 
+        # if we pass the number of documents we want to read then stop
         if docs > numDocuments:
             break
 
+        # split the file line to a list and get the first 2 columns (docID and wordID)
         list = data[i].split(" ")
         docID = list[0]
         wordID = list[1]
+
+        # we need it to write after in file
+        string = string + [docID + " : " + wordID + "\n"]
+
+        # add tuple of docID and wordID in list
         frozenData = frozenData + [(docID, wordID)]
 
-        if nextDocID < int(docID):
+        # check if we changed docID then increase the docs number to check it for the next
+        if previousDocID < int(docID):
             docs += 1
 
-        nextDocID = int(docID)
+        previousDocID = int(docID)
 
+
+    # remove the last tuple from list because its not in range of the number of documents
     frozenData.pop(-1)
-    frozenData.pop(-1)
-    print(frozenData)
 
-    frozeSet = frozenset(frozenData)
-    print(frozeSet)
+    # convert list of tuples to frozenset
+    frozenSet = frozenset(frozenData)
 
+    # no need we remove the last element
+    string.pop(-1)
 
+    # convert list to string to save it in file
+    fstring = listToString(string)
 
-
-
-
+    # write the string in file
+    newFile.write(fstring)
 
 
 
